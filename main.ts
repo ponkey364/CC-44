a58771a5178c88085b343646a8eee17b1fcbe9a1
Ben Thomas <ponkey@rpgpn.co.uk>
2019-03-27 10:56:43 +0000

    License

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+/*Copyright(C) 2019  Ponkey364
+
+This program is free software: you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
 /**
  * Note that this program cannot solve massively complex puzzles. It'll probably solve the 'easy' one in your daily
  * newspaper of choice, but it won't be doing anything more difficult than that.
